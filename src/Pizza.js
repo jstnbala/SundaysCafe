17c34dcd8d4a0114460f495e5f90447b43557846
Justin <justinbala339@gmail.com>
2024-04-21 00:01:14 +0800

    Added other Navs

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,11 +1,35 @@
-// Pizza.js
 import React from 'react';
 
 const Pizza = () => {
+  const pizzaItems = [
+    {
+      name: 'Prosciutto and Arugula Pizza',
+      description: 'Delicious pizza topped with prosciutto, fresh arugula, and tangy tomato sauce.',
+      image: 'Prosciutto.png'
+    },
+    {
+      name: 'The Cheesy Cinco Queso Pizza',
+      description: 'A cheesy delight with five different kinds of cheeses, melted to perfection.',
+      image: 'CincoQueso.png'
+    },
+    {
+      name: 'Pesto Mushroom Bacon Pizza',
+      description: 'Savory pizza featuring pesto sauce, mushrooms, crispy bacon, and gooey cheese.',
+      image: 'PestoPizza.png'
+    }
+  ];
+
   return (
-    <div>
-      <h2>Welcome to Pizza</h2>
-      <p>This is the Pizza Page of Sundays Cafe Restaurant.</p>
+    <div className="pizza-container">
+      {pizzaItems.map((pizza, index) => (
+        <div key={index} className="pizza-card">
+          <img src={`./images/${pizza.image}`} alt={pizza.name} className="pizza-image" />
+          <div className="pizza-details">
+            <h3 className="pizza-name">{pizza.name}</h3>
+            <p className="pizza-description">{pizza.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
